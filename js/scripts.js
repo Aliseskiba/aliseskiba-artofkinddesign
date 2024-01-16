f4a20375a8aa9dd4db69bf7a44d8bf71ca4d3484
Aliseskiba <alise@artofkind.design>
2024-01-16 12:26:56 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 var Welcome = " Welcome to Alise's portfolio site!";
 console.log(Welcome);
+
+function menuToggle() {
+    var x = document.getElementById ('myNavtoggle');
+    if (x.className === 'navtoggle'){
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
